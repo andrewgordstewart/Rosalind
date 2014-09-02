24dc5020ca4e343b23b9baacf15cc094e71682da
kaptainkayak <kaptainkayak@gmail.com>
2014-09-01 23:06:07 -0400

    solved the longest_common_substring problem

--- bin/longest_common_substring.py
+++ bin/longest_common_substring.py
@@ -1,14 +1,39 @@
-import SuffixTree.SuffixTree
+from rosalind.iotools import read_complete_fasta as read_fasta
+from rosalind.iotools import copy_to_clipboard
 
-s = SuffixTree.SuffixTree()
+with open('../data/rosalind_lcsm.txt', 'r') as fp:
+    strings = [c for i, c in read_fasta(fp)]
 
-s.add('hello world', 1)
-s.add('otello won', 2)
 
-print dir(SuffixTree.SuffixTree), '\n', '-'*20
+def longest_common_substring(strings):
+    strings = sorted(strings)
+    short_string = strings[0]
+    other_strings = strings[1:]
 
-print dir(SuffixTree.SuffixNode)
+    # print 'here'
 
-x = s.root()
+    l = len(short_string)
+    m = ''
+    for i in range(0, l):
+        for j in range(l, i + len(m), -1):
+            s1 = short_string[i:j]
 
-print x.stree_get_children()
+            matched_all = True
+            # print 'checking %s' % s1
+            for s2 in other_strings:
+                if s1 not in s2:
+                    matched_all = False
+                    break
+
+            if matched_all:
+                m = s1
+                break
+
+    return m
+
+
+if __name__ == "__main__":
+
+    longest_common_substring = longest_common_substring(strings)
+    print longest_common_substring
+    copy_to_clipboard(longest_common_substring)
