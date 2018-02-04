5facdec31c4d389b80850baed547a729f56109f1
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-02-04 14:30:44 -0300

    Use Solution in problem 01

--- solutions/01_counting_dna_nucleotides.py
+++ solutions/01_counting_dna_nucleotides.py
@@ -1,31 +1,12 @@
 import sys
-import timeit
 
-import rosalind.genetools
-
-
-def solve():
-    with open('./data/input/01.txt', 'r') as f_in:
-        dna_string = f_in.read().strip()
-
-        with open('./data/output/01.txt', 'w+') as f_out:
-            for codon in rosalind.genetools.dna_profile(dna_string):
-                f_out.write(str(codon))
-                f_out.write(' ')
+from rosalind.genetools import dna_profile
+from rosalind.solution import Solution
 
 
-if "test" in sys.argv:
-    sample_in = input("Please enter the sample input:\n")
-    sample_out = input("Please enter the sample output:\n")
+def solve(dna_string):
+    return ' '.join(str(num) for num in dna_profile(dna_string.strip()))
 
-    answer = rosalind.genetools.dna_profile(sample_in)
 
-    try:
-        assert sample_out == ' '.join(str(num) for num in answer)
-    except AssertionError:
-        print(sample_out, answer)
-    else:
-        print("Success")
-else:
-    secs = timeit.timeit("solve()",  setup="from __main__ import solve", number=1)
-    print(f"Solved in {secs} seconds")
+if __name__ == "__main__":
+    Solution(solve, "01").solve(test="test" in sys.argv)
