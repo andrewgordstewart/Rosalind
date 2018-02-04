296a7d09692961684840212d7bb0b5e1185dd8a4
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-02-04 14:38:59 -0300

    Use PROBLEM_PREFIX instead of magic constant

--- solutions/01_counting_dna_nucleotides.py
+++ solutions/01_counting_dna_nucleotides.py
@@ -3,10 +3,12 @@ import sys
 from rosalind.genetools import dna_profile
 from rosalind.solution import Solution
 
+PROBLEM_PREFIX = "01"
+
 
 def solve(dna_string):
     return ' '.join(str(num) for num in dna_profile(dna_string.strip()))
 
 
 if __name__ == "__main__":
-    Solution(solve, "01").solve(test="test" in sys.argv)
+    Solution(solve, PROBLEM_PREFIX).solve(test="test" in sys.argv)
