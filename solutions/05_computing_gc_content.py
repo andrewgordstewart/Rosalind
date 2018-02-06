f554ebc89e7d1a4548f6c6592470ad7f799db5f4
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-02-05 22:17:55 -0300

    Solve computing_gc_content

--- solutions/05_computing_gc_content.py
+++ solutions/05_computing_gc_content.py
@@ -1,16 +1,15 @@
 from rosalind.iotools import read_fasta
 from rosalind.genetools import gc_content
 
-fasta = {}
-with open('../data/rosalind_gc.txt') as fp:
-    for name, seq in read_fasta(fp):
+
+def solve(dataset):
+    fasta = {}
+    for name, seq in read_fasta(dataset):
         fasta[name] = seq
-        # fasta.append((name, seq))
-    fp.close
 
-max_gc = 0
-for key in fasta:
-    if gc_content(fasta[key]) > max_gc:
-        max_gc, max_key = gc_content(fasta[key]), key
+    max_gc = 0
+    for key in fasta:
+        if gc_content(fasta[key]) > max_gc:
+            max_gc, max_key = gc_content(fasta[key]), key
 
-print max_key, max_gc
+    return "\n".join((max_key, str(100*max_gc)))
