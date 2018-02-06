7ecec47bc2e3cbd9f9a04c901aeb5a914c811e21
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-02-05 22:36:34 -0300

    Solve mendels_first_law

--- solutions/07_mendels_first_law.py
+++ solutions/07_mendels_first_law.py
@@ -2,7 +2,6 @@ def is_dominant_prob(num_homo_dom, num_hetero, num_homo_rec):
     total = num_homo_dom + num_hetero + num_homo_rec
     total = total*(total-1)
 
-
     # prob(at least one mate is homo_dom):
     p1 = num_homo_dom*(num_homo_dom-1
                        + 2*num_hetero
@@ -17,6 +16,10 @@ def is_dominant_prob(num_homo_dom, num_hetero, num_homo_rec):
 
     return p1 + .75*p2 + .5*p3
 
-print is_dominant_prob(2, 2, 2)
 
-print is_dominant_prob(17, 20, 22)
+def solve(dataset):
+    args = [int(num) for num in dataset.rstrip().split(" ")]
+    if len(args) != 3:
+        raise ValueError(f"Invalid input {args}")
+
+    return is_dominant_prob(*args)
