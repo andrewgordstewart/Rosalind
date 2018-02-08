9df4cdfe9777884bd1121492b5153f6a3260c52b
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-02-08 16:43:14 -0300

    Solve overlap graphs

--- solutions/12_overlap_graphs.py
+++ solutions/12_overlap_graphs.py
@@ -1,15 +1,15 @@
 from rosalind.genetools import does_overlap
-from rosalind.iotools import read_complete_fasta as _read_fasta
-
-with open('../data/rosalind_grph.txt', 'r') as fp:
-    fasta = _read_fasta(fp)
+from rosalind.iotools import read_fasta as read_fasta
 
 k = 3
-for s in fasta:
-    for t in fasta:
-        if does_overlap(s[1], t[1], k):
-            print s[0][1:], t[0][1:]
 
 
+def solve(dataset):
+    fasta = [f for f in read_fasta(dataset)]
+    result = []
+    for s in fasta:
+        for t in fasta:
+            if does_overlap(s[1], t[1], k):
+                result.append(s[0] + ' ' + t[0])
 
-
+    return '\n'.join(result)
