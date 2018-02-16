966e511f0a4ff42598baa74474f9ce3dd5da4bea
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-02-16 08:23:56 -0500

    Solve Enumerating Gene Orders

--- solutions/19_enumerating_gene_orders.py
+++ solutions/19_enumerating_gene_orders.py
@@ -1,23 +1,9 @@
 from itertools import permutations
 
-n = 7
-p = permutations(range(n), n)
 
-fp = open('../data/permutations.txt', 'w')
+def solve(dataset):
+    n = int(dataset.strip())
+    perms = permutations(range(n), n)
 
-count = 0
-result = ''
-for perm in p:
-    subcount = 0
-    for i in perm:
-        result = result + str(i+1)
-        if subcount < n:
-            result = result + ' '
-    result = result + '\n'
-    count += 1
-
-print count
-print result
-
-fp.write(str(count) + '\n')
-fp.write(result)
+    perms = [' '.join(str(i+1) for i in perm) for perm in perms]
+    return str(len(perms)) + '\n' + '\n'.join(perms)
