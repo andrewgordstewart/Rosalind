c57ce104216a9e0a9194368321a12743978f5aef
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-02-16 08:10:20 -0500

    Solve Open Reading Frames

--- solutions/18_open_reading_frames.py
+++ solutions/18_open_reading_frames.py
@@ -1,20 +1,12 @@
-from rosalind.iotools import read_complete_fasta as read_fasta
+from rosalind.iotools import read_fasta
 from rosalind.genetools import candidate_proteins
 
-with open('../data/rosalind_orf.txt', 'r') as fp:
-    dna = read_fasta(fp)[0][1]
 
+def solve(dataset):
+    dna = list(read_fasta(dataset))[0][1]
 
-proteins = candidate_proteins(dna, 'dna')
-answer = ''
-for p in proteins:
-    answer = answer + p + '\n'
+    proteins = candidate_proteins(dna, 'dna')
+    answer = ''.join(p + '\n' for p in proteins)
 
-
-answer.rstrip()
-print answer
-
-# from rosalind.iotools import copy_to_clipboard
-
-# copy_to_clipboard(answer)
-
+    answer.rstrip()
+    return answer
